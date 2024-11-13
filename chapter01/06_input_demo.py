3270457ec5b278e5a8c3badb1d84d5eb28648090
DimDrk <dimdrk@gmail.com>
2024-11-13 13:39:06 +0200

    input_demo

--- chapter01/06_input_demo.py
+++ chapter01/06_input_demo.py
@@ -4,3 +4,10 @@ is_student = input("Are you a student? (yes/no): ").lower() == "yes"
 
 print(f"name: {name}")
 print(f"is_student: {is_student}")
+
+if is_student:
+    print("You are a student.")
+else:
+    print("You are not a student.")
+
+    
