c85a2a3c32b833cdf9c603422a029badb7f82308
DimDrk <dimdrk@gmail.com>
2024-11-14 00:28:52 +0200

    str clicing

---
+++ chapter01/21_str_clicing.py
@@ -0,0 +1,29 @@
+s = "Coding Factory"
+
+s1 = s[7]
+print(s1)  # F
+
+s2 = s[:6]
+print(s2)  # Coding
+
+s3 = s[7:]
+print(s3)  # Factory
+
+s4 = s[:]
+print(s4) # Coding Factory
+
+s5 = s[-1]
+print(s5) # y
+
+s6 = s[:-2]
+print(s6) # Coding Facto
+
+# [start:stop:step]
+s7 = s[::2]
+print(s7)   # Cdn atr
+
+s8 = s[-1::-1]
+print(s8)   #Coding Factory (reversed)
+
+s9 = s[::-1]
+print(s9)   #Coding Factory (reversed)
