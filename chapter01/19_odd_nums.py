9b759f61b96d3a3ef3967932ecfb12950720294f
DimDrk <dimdrk@gmail.com>
2024-11-13 22:37:25 +0200

    odd nums

---
+++ chapter01/19_odd_nums.py
@@ -0,0 +1,16 @@
+# Java Style
+for i in range(22):
+    if i % 2 != 0:
+        print(i, end=" ")
+print()
+
+# Python Style
+for i in range(1, 22, 2):
+    print(i, end=" ")
+print()
+
+for i in range(100):
+    if i == 50:
+        break
+
+print(i)
