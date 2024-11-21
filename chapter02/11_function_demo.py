a368d436cdcf31810362aa9613ba460a78339efd
DimDrk <dimdrk@gmail.com>
2024-11-21 21:05:08 +0200

    function demo

---
+++ chapter02/11_function_demo.py
@@ -0,0 +1,16 @@
+def say_hello(message):
+    """
+    Prints message!
+
+    Args:
+        message: the message which is going to be printed.
+    """
+    # print("Hello super team!")
+    print(message)
+#    return 10                   # default value -> none
+
+def main():
+    say_hello("Hello again")
+
+if __name__ == "__name__":
+    main()
