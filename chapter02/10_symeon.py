585b25b7ec2ac4045e69692104d444d75922767c
DimDrk <dimdrk@gmail.com>
2024-11-21 20:40:15 +0200

    question module

---
+++ chapter02/10_symeon.py
@@ -0,0 +1,10 @@
+sym = {
+    #{1, 2, 3} : "Hello",    # TypeError: unhashable type: 'set'
+    frozenset({1, 2, 3}) : "Hello",
+    "key1": "value1",
+    "key2": 10,
+    3: [1, 2, 3],
+    4: {1: "one", 2: "two"}
+}
+
+print(sym)  
