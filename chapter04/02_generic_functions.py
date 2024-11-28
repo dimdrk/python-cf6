3c3d74fc4a520d2134d1624a52e1d4aa3aec2c1d
DimDrk <dimdrk@gmail.com>
2024-11-28 12:13:37 +0200

    generic functions

---
+++ chapter04/02_generic_functions.py
@@ -0,0 +1,34 @@
+from typing import TypeVar
+
+# Declare Generic Type Variable
+t = TypeVar('T')
+
+Number = TypeVar("Number", int, float)
+
+def add_numbers(a: Number, b: Number) -> Number:
+    """
+    Adds two numbers (int or float) and returns the result.
+
+    Args:
+        a (Number): The first number.
+        b (Number): The second number.
+
+    Returns:
+        Number: The sum of two numbers.
+    """
+    return a + b
+
+def main():
+    int_sum = add_numbers(10, 20)
+    print(f"int_sum: {int_sum}")
+
+    float_sum = add_numbers(5.2, 2.3)
+    print(f"float_sum: {float_sum}")
+
+    mixin_sum = add_numbers(10.2, 15)
+    print(f"mixin_sum: {mixin_sum}")
+
+    fail_sum = add_numbers("Hello", " CF6")     # works... 
+
+if __name__ == "__main__":
+    main()
