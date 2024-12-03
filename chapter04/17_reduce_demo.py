7dcda5fe5253318f5a0431b18f28dbbe6099fccf
DimDrk <dimdrk@gmail.com>
2024-12-03 19:43:49 +0200

    reduce demo

--- chapter04/17_reduce_demo.py
+++ chapter04/17_reduce_demo.py
@@ -5,5 +5,7 @@ my_ints = [1, 2, 3, 4, 5]
 
 # return a result: 1 * 2 * 3 * 4 * 5
 result = reduce(lambda x, y: x * y, my_ints)
-
 print(result)
+
+result2 = reduce(lambda x, y: x * y, my_ints, 1)
+print(result2)
