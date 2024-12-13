5c36fc2ba0e18f26aa2fb8ae1f13c3e5a811ed40
DimDrk <dimdrk@gmail.com>
2024-12-13 11:30:13 +0200

    hashable demo

---
+++ chapter05/29_hashable_demo.py
@@ -0,0 +1,36 @@
+class Point:
+    def __init__(self, x, y):
+        self.__x = x
+        self.__y = y
+
+    def __eq__(self, other):
+        return isinstance(other, Point) and self.__x == other.__x and self.__y == other.__y
+    
+    def __hash__(self):
+        return hash((self.__x, self.__y))
+    
+    def __str__(self):
+        return f"Point({self.__x}, {self.__y})"
+
+
+def main():
+    p1 = Point(1, 2)
+    p2 = Point(3, 4)
+    p3 = Point(1, 2)
+
+    point_dict = {
+        p1: "Point 1",
+        p2: "Point 2",
+        p3: "Point 3"
+    }
+
+    print(hash(p1))
+    print(hash(p2))
+    print(hash(p3))
+    print(p1 == p3)
+
+    for key, value in point_dict.items():
+        print(f"{key}:{value}")
+
+if __name__ == '__main__':
+    main()
